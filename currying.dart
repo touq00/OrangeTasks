2985030d6e39009c9760755b9fd7467726f91d29
Ayoub Touq <abutouq_k@yahoo.com>
2023-10-27 13:38:41 +0300

    modified

--- currying.dart
+++ currying.dart
@@ -5,17 +5,20 @@ int  add(int a, int b, int c, int d){
   return a + b + c + d;
 }
 
-int Function(int) Function(int) Function(int) addCurrying(int a){
-  return (int b) {
-    return (int c) {
-      return (int d) {
-        return add(a ,b ,c,d);
+ Function(int)? addCurrying(int a){
+   Function(int) inner1= (int b){
+     Function(int) inner2 = (int c){
+       Function(int) inner3 = (int d){
+        return add(a,b,c,d);
       };
+      return inner3;
     };
+    return inner2;
   };
+  return inner1;
 };
 final addTwo = addCurrying(2);
-final addThree = addTwo(5);
+final addThree = addTwo!(5);
 final addFour = addThree(3);
 final res = addFour(4);
 print(res);
